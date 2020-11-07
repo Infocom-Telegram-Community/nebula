fb4765cbe31989915c2277d588c79b4df5b759a0
hersel91 <hersel1991@gmail.com>
2020-11-07 22:51:50 +0100

    Release 7.8.3

--- core/modules/commands/owner/broadcast.py
+++ core/modules/commands/owner/broadcast.py
@@ -11,4 +11,4 @@ def init(update, context):
     rows = connector.cur.fetchall()
     for a in rows:
         chatid = a[1]
-        bot.send_message(chatid,message,parse_mode='HTML')
+        bot.send_message(chatid,"<b>BROADCAST:</b> {}".format(message),parse_mode='HTML')
