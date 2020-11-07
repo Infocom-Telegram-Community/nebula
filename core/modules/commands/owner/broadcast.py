fad08a2ec6d686046de52c69a89bf273b0d02d36
hersel91 <hersel1991@gmail.com>
2020-11-07 22:25:00 +0100

    Release 7.8.0

--- core/modules/commands/owner/broadcast.py
+++ core/modules/commands/owner/broadcast.py
@@ -1,7 +1,7 @@
 import core.decorators
 from core.sql.db_connect import Connection
 
-#@core.decorators.owner.init
+@core.decorators.owner.init
 def init(update, context):
     bot = context.bot
     message = update.message.text[2:]
@@ -11,5 +11,6 @@ def init(update, context):
     connector.cur.execute(query,[chat])
     rows = connector.cur.fetchall()
     for group in rows:
+        print(group[1])
         chatid = group[1]
         bot.send_message(chatid,message,parse_mode='HTML')
